33aad22891cf9c1af19d1fe3550ea20dd33c712c
MohamedIbrahim768 <mohamedmo201108@gmail.com>
2025-12-13 07:50:58 +0200

    this file is for curling the website to get data

---
+++ bitcoincollect.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+json=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+
+price=$(echo "$json" | awk -F'"usd":' '{print $2}'| tr -d '}')
+
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ -z "$price" ];then
+        echo "$timestamp - Error: Failed to retrieve price."
+
+else
+
+echo "$timestamp - $price"
+
+echo "$timestamp,$price" >> bitcoin_prices.csv
+
+gnuplot /home/miai1e25/plot_bitcoin.gp
+
+fi
+[miai1e25@linuxproj ~]$
